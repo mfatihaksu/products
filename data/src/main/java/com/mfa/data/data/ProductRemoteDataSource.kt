56eef90e4eae35e56a50b22d5fdb9ee6bb4bbf89
Mehmet Fatih Aksu <dev.mehmetfatihaksu@gmail.com>
2024-07-14 16:54:03 +0300

    remove unnecessary flow

--- data/src/main/java/com/mfa/data/data/ProductRemoteDataSource.kt
+++ data/src/main/java/com/mfa/data/data/ProductRemoteDataSource.kt
@@ -2,28 +2,11 @@ package com.mfa.data.data
 
 import com.mfa.data.helper.ApiClient
 import com.mfa.data.helper.ApiOperation
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ProductRemoteDataSource @Inject constructor(
     private val apiClient: ApiClient
 ) : IProductRemoteDataSource {
-    override val products: Flow<List<Product>>
-        get() = channelFlow {
-            getProducts()
-                .onSuccess { products ->
-                    CoroutineScope(Dispatchers.IO).launch {
-                        send(products.products)
-                    }
-                }.onFailure {
-
-                }
-        }.flowOn(Dispatchers.IO)
 
     override suspend fun getProducts(): ApiOperation<Products> {
         return apiClient.getProducts()
@@ -35,7 +18,6 @@ class ProductRemoteDataSource @Inject constructor(
 }
 
 interface IProductRemoteDataSource {
-    val products: Flow<List<Product>>
     suspend fun getProducts(): ApiOperation<Products>
     suspend fun getProduct(id: String): ApiOperation<Product>
 }
